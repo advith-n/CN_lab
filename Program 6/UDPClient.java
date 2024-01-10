4b0c74e7c5f959327a296d622339f5267c4a6973
advith-n <109013191+advith-n@users.noreply.github.com>
2024-01-10 06:07:05 +0530

    Update UDPClient.java

--- Program 6/UDPClient.java
+++ Program 6/UDPClient.java
@@ -1,25 +1,30 @@
-import java.util.*;
+import java.io.*; 
 import java.net.*;
+public class UDPC
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader inPut=new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSock=new DatagramSocket();
+        InetAddress ipAdd=InetAddress.getByName("localhost");
 
-public class UDPClient {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        DatagramSocket dataSock = new DatagramSocket(6000);
-        byte[] buffer;
-        String msg;
-        System.out.println("Recived Message : ");
-        while (true) {
-            buffer = new byte[65555];
-            DatagramPacket dataPack = new DatagramPacket(buffer, buffer.length);
-            dataSock.receive(dataPack);
-            msg = new String(buffer).trim();
-            System.out.println(msg);
-            if (msg.equalsIgnoreCase("exit")) {
-                System.out.println("End Of Message");
-                break;
-            }
-            sc.close();
-            dataSock.close();
-        }
+        byte[] sendData = new byte[1024];
+        byte[] recvData = new byte[1024];
+
+        System.out.println("Enter the string in lowercase so that you receive the message in Uppercase from the server");
+        String sentence=inPut.readLine();
+
+        sendData=sentence.getBytes();
+
+        DatagramPacket sendPack = new DatagramPacket(sendData, sendData.length,ipAdd, 9884);
+        clientSock.send(sendPack);
+
+        DatagramPacket recvPack=new DatagramPacket(recvData, recvData.length);
+        clientSock.receive(recvPack);
+
+        String modifiedSentence=new String(recvPack.getData());
+        System.out.println("FROM SERVER: "+modifiedSentence);
+
+        clientSock.close();
     }
 }
